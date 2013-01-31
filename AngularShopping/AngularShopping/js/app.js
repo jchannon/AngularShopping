8d8cb28c6b129bc0d2a9b9465500963e35420784
Jonathan Channon <jonathan.channon@gmail.com>
2013-01-31 12:20:11 +0000

    Added Directive for when something is added to the basket

--- AngularShopping/AngularShopping/js/app.js
+++ AngularShopping/AngularShopping/js/app.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     'use strict';
 
-    var app = angular.module('myCart', []).
+    var app = angular.module('myCart', ['ui']).
         config(function ($routeProvider) {
             $routeProvider.
                 when('/', {
@@ -18,40 +18,40 @@
     app.factory('shoppingItemsService', function () {
         var data = [
             {
-                "id": 1,
-                "title": "The Door",
-                "artist": "Religious Knives",
-                "image": "http://ecx.images-amazon.com/images/I/51og8BkN8jL._SS250_.jpg",
-                "large_image": "http://ecx.images-amazon.com/images/I/51og8BkN8jL._SS500_.jpg",
-                "price": 9.98,
-                "url": "http://www.amazon.com/Door-Religious-Knives/dp/B001FGW0UQ/?tag=quirkey-20"
+                'id': 1,
+                'title': 'The Door',
+                'artist': 'Religious Knives',
+                'image': 'http://ecx.images-amazon.com/images/I/51og8BkN8jL._SS250_.jpg',
+                'large_image': 'http://ecx.images-amazon.com/images/I/51og8BkN8jL._SS500_.jpg',
+                'price': 9.98,
+                'url': 'http://www.amazon.com/Door-Religious-Knives/dp/B001FGW0UQ/?tag=quirkey-20'
             },
             {
-                "id": 2,
-                "title": "Album",
-                "artist": "Girls",
-                "image": "http://ecx.images-amazon.com/images/I/51hDxOeIeML._SS250_.jpg",
-                "large_image": "http://ecx.images-amazon.com/images/I/51hDxOeIeML._SS500_.jpg",
-                "price": 13.98,
-                "url": "http://www.amazon.com/gp/product/B002GNOMJE?ie=UTF8&tag=quirkeycom-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B002GNOMJE"
+                'id': 2,
+                'title': 'Album',
+                'artist': 'Girls',
+                'image': 'http://ecx.images-amazon.com/images/I/51hDxOeIeML._SS250_.jpg',
+                'large_image': 'http://ecx.images-amazon.com/images/I/51hDxOeIeML._SS500_.jpg',
+                'price': 13.98,
+                'url': 'http://www.amazon.com/gp/product/B002GNOMJE?ie=UTF8&tag=quirkeycom-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B002GNOMJE'
             },
             {
-                "id": 3,
-                "title": "Bitte Orca",
-                "artist": "The Dirty Projectors",
-                "image": "http://z2-ec2.images-amazon.com/images/P/B0026T4RTI.01._SS250_.jpg",
-                "large_image": "http://z2-ec2.images-amazon.com/images/P/B0026T4RTI.01._SS500_.jpg",
-                "price": 13.98,
-                "url": "http://www.amazon.com/Bitte-Orca-Dirty-Projectors/dp/B0026T4RTI/ref=pd_sim_m_12?tag=quirkey-20"
+                'id': 3,
+                'title': 'Bitte Orca',
+                'artist': 'The Dirty Projectors',
+                'image': 'http://z2-ec2.images-amazon.com/images/P/B0026T4RTI.01._SS250_.jpg',
+                'large_image': 'http://z2-ec2.images-amazon.com/images/P/B0026T4RTI.01._SS500_.jpg',
+                'price': 13.98,
+                'url': 'http://www.amazon.com/Bitte-Orca-Dirty-Projectors/dp/B0026T4RTI/ref=pd_sim_m_12?tag=quirkey-20'
             },
             {
-                "id": 4,
-                "title": "The Pains of Being Pure at Heart",
-                "artist": "The Pains of Being Pure at Heart",
-                "image": "http://z2-ec2.images-amazon.com/images/P/B001LGXIDS.01._SS250_.jpg",
-                "large_image": "http://z2-ec2.images-amazon.com/images/P/B001LGXIDS.01._SS500_.jpg",
-                "price": 13.99,
-                "url": "http://www.amazon.com/Pains-Being-Pure-Heart/dp/B001LGXIDS/ref=pd_sim_m_44?tag=quirkey-20"
+                'id': 4,
+                'title': 'The Pains of Being Pure at Heart',
+                'artist': 'The Pains of Being Pure at Heart',
+                'image': 'http://z2-ec2.images-amazon.com/images/P/B001LGXIDS.01._SS250_.jpg',
+                'large_image': 'http://z2-ec2.images-amazon.com/images/P/B001LGXIDS.01._SS500_.jpg',
+                'price': 13.99,
+                'url': 'http://www.amazon.com/Pains-Being-Pure-Heart/dp/B001LGXIDS/ref=pd_sim_m_44?tag=quirkey-20'
             }
         ];
 
@@ -65,6 +65,8 @@
 
     app.factory('basketService', function () {
         return {
+
+
             getCount: function () {
                 var basket = JSON.parse((localStorage.getItem('shoppingBasket') || '{ "items": [] }'));
                 var count = 0;
@@ -80,12 +82,14 @@
 
                 var itemStoredAlready = basket.items.filter(function (x) { return x.id === item.id; }).length === 1;
 
+                var basketItem = {};
+
                 if (itemStoredAlready) {
-                    var basketItem = basket.items.filter(function (x) { return x.id === item.id; })[0];
+                    
+                    basketItem = basket.items.filter(function (x) { return x.id === item.id; })[0];
                     basketItem.quantity += parseInt(item.quantity, 10);
                 } else {
 
-                    var basketItem = {};
                     basketItem.id = item.id;
                     basketItem.title = item.title;
                     basketItem.quantity = parseInt(item.quantity, 10);
@@ -97,4 +101,23 @@
             }
         };
     });
+
+    app.directive('quantity', function () {
+        var linker = function (scope, element, attrs, basketService) {
+           
+            scope.$watch(attrs.quantity, function (value, oldValue) {
+                if (value > oldValue) {
+                    element
+                    .animate({ paddingTop: '30px' })
+                    .animate({ paddingTop: '10px' });
+                }
+            }, true);
+        };
+
+        return {
+            restrict: 'A',
+            link: linker
+        };
+    });
+
 })();
